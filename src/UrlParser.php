5b05694f3a1c7e041e56e2c8efdbb3cb63f2e312
MrAnyx <1ptitigre@gmail.com>
2020-10-16 17:33:47 +0200

    [feat] Added basic file

--- src/UrlParser.php
+++ src/UrlParser.php
@@ -4,4 +4,59 @@ namespace TimePHP\UrlParser;
 
 class Parser {
    
+
+   /**
+    * Url pass un parameter  
+    *
+    * @var string
+    */
+   private $url;
+
+   /**
+    * Method of the current request (GET, POST, PUT, HEAD)
+    *
+    * @var string
+    */
+   private $method;
+
+   /**
+    * Domain name
+    *
+    * @var string
+    */
+   private $domain;
+
+   public function __construct(){
+      $this->url = $_SERVER["PATH_INFO"];
+      $this->method = $_SERVER["REQUEST_METHOD"];
+      $this->domain = $_SERVER["SERVER_NAME"];
+   }
+
+   /**
+    * return the current url
+    *
+    * @return string
+    */
+   public function getUrl(): string {
+      return $this->url;
+   }
+
+
+   /**
+    * Return the current request method (GET, POST, HEAD, PUT)
+    *
+    * @return string
+    */
+   public function getMethod(): string {
+      return $this->method;
+   }
+
+   public function getDomain(): string {
+      return $this->domain;
+   }
+
+
+
+
+
 }
